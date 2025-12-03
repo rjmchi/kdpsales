292d795f1540731aaba35b156617cdf18ed03988
rjmchi <robert@rjmprogramming.com>
2025-12-03 17:52:54 -0600

    Changes royalty to sale->royalty

--- resources/views/bydate.blade.php
+++ resources/views/bydate.blade.php
@@ -1,44 +1,44 @@
 <x-guest-layout>
 
-    <div class="m-2 p-2">
+    <div class="p-2 m-2">
         @php
             $total = 0;
             $total_royalty = 0;
         @endphp
         @foreach ($sales as $sale)
-            <div class="border-2 rounded p-2 m-2  bg-indigo-50">
-                <table class="table-fixed w-full">
+            <div class="p-2 m-2 border-2 rounded bg-indigo-50">
+                <table class="w-full table-fixed">
                     @php
                         $total += $sale->qty;
-                        $royalty = $sale->book->royalty * $sale->qty;
-                        $total_royalty += $royalty;
+                        $total_royalty += $sale->royalty;
                     @endphp
                     <tr class="border-b-2">
-                        <td>{{$sale->book->title}}</td>
-                        <td class="pl-4">{{$sale->order_date}}</td>
-                        <td>{{$sale->qty}}</td>
-                        <td>{{$royalty}}</td>
+                        <td>{{ $sale->book->title }}</td>
+                        <td class="pl-4">{{ $sale->order_date }}</td>
+                        <td>{{ $sale->qty }}</td>
+                        <td>{{ $sale->royalty }}</td>
                     </tr>
 
                 </table>
             </div>
         @endforeach
-        <table class="table-fixed w-full">
+        <table class="w-full table-fixed">
             <tr class="font-bold border-t-2">
                 <td>Total:</td>
-                <td>{{$total}}</td>
-                <td>{{$total_royalty}}</td>
+                <td>{{ $total }}</td>
+                <td>{{ $total_royalty }}</td>
             </tr>
         </table>
     </div>
 
     <div class="flex">
-        <div class="border-2 bg-white m-3 p-2">
+        <div class="p-2 m-3 bg-white border-2">
             <h2 class="text-lg font-bold">Load</h2>
-            <form action="{{route('loadBooks')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('loadBooks') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <input type="file" name="file" class="form-control">
-                <button type="submit" class="ml-8 rounded-md bg-indigo-700 px-3 py-2 leading-5 text-white hover:bg-indigo-600">Upload</button>
+                <button type="submit"
+                    class="px-3 py-2 ml-8 leading-5 text-white bg-indigo-700 rounded-md hover:bg-indigo-600">Upload</button>
             </form>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,56 @@
 <x-guest-layout>
 
-    <div class="m-2 p-2">
+    <div class="p-2 m-2">
         @php
             $grand_total = 0;
             $total_books_sold = 0;
         @endphp
         @foreach ($books as $book)
-            <div class="border-2 rounded p-2 m-2  bg-indigo-50">
-                <h2 class="text-xl font-bold mt-2">{{$book->title}}</h2>
-                <table class="table-fixed w-full">
-                @php
-                    $total = 0;
-                    $total_royalty = 0;
-                @endphp
-
-                @foreach ($book->sales as $sale)
+            <div class="p-2 m-2 border-2 rounded bg-indigo-50">
+                <h2 class="mt-2 text-xl font-bold">{{ $book->title }}</h2>
+                <table class="w-full table-fixed">
                     @php
-                        $total += $sale->qty;
-                        $royalty = $book->royalty * $sale->qty;
-                        $total_royalty += $royalty;
-                        $grand_total += $royalty;
-                        $total_books_sold += $sale->qty;
+                        $total = 0;
+                        $total_royalty = 0;
                     @endphp
-                    <tr class="border-b-2">
-                        <td class="pl-4">{{$sale->order_date}}</td>
-                        <td>{{$sale->qty}}</td>
-                        <td>{{$royalty}}</td>
-                    </tr>
-                @endforeach
+
+                    @foreach ($book->sales as $sale)
+                        @php
+                            $total += $sale->qty;
+                            $total_royalty += $sale->royalty;
+                            $grand_total += $sale->royalty;
+                            $total_books_sold += $sale->qty;
+                        @endphp
+                        <tr class="border-b-2">
+                            <td class="pl-4">{{ $sale->order_date }}</td>
+                            <td>{{ $sale->qty }}</td>
+                            <td>{{ $sale->royalty }}</td>
+                        </tr>
+                    @endforeach
                     <tr>
                         <td>Total:</td>
-                        <td>{{$total}}</td>
-                        <td>{{$total_royalty}}</td>
+                        <td>{{ $total }}</td>
+                        <td>{{ $total_royalty }}</td>
                     </tr>
                 </table>
             </div>
         @endforeach
-        <table class="table-fixed w-full">
+        <table class="w-full table-fixed">
             <tr class="font-bold border-t-2">
                 <td>Grand Total</td>
-                <td>{{$total_books_sold}}</td>
-                <td>{{$grand_total}}</td>
+                <td>{{ $total_books_sold }}</td>
+                <td>{{ $grand_total }}</td>
             </tr>
         </table>
     </div>
 
     <div class="flex">
-        <div class="border-2 bg-white m-3 p-2">
+        <div class="p-2 m-3 bg-white border-2">
             <h2 class="text-lg font-bold">Load</h2>
-            <p class="text-sm text-indigo-900 italic mb-2">Export the Paperback Royalty tab as CSV before importing. <br>Save in MyDocuments/KDP Reports</p>
+            <p class="mb-2 text-sm italic text-indigo-900">Export the Paperback Royalty tab as CSV before importing.
+                <br>Save in MyDocuments/KDP Reports</p>
 
-            <form action="{{route('loadBooks')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('loadBooks') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <input type="file" name="file" class="form-control">
                 <x-primary-button>Upload</x-primary-button>
